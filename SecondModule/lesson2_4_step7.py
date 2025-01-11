ce52a16e35e72cc0811684d01c7008f4a93bc938
Илья <vitilyusha@gmail.com>
2025-01-11 15:23:29 +0300

    Explicit Waits

--- SecondModule/lesson2_4_step7.py
+++ SecondModule/lesson2_4_step7.py
@@ -10,7 +10,9 @@ browser.get("http://suninjuly.github.io/wait1.html")
 
 # time.sleep(1)
 
-button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable(By.ID, "verify"))
+button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.ID, "verify"))
+    )
 button.click()
 message = browser.find_element(By.ID, "verify_message")
 
