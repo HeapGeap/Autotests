93b81fa34a43abacb1b0e39e47a2f2d82340c2e9
Илья <vitilyusha@gmail.com>
2025-01-11 15:02:48 +0300

    Implicit wait

--- SecondModule/lesson2_4_step3.py
+++ SecondModule/lesson2_4_step3.py
@@ -1,8 +1,14 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome()
 browser.get("http://suninjuly.github.io/wait1.html")
+
+# time.sleep(1)
+
+browser.implicitly_wait(5)
 
 button = browser.find_element(By.ID, "verify")
 button.click()
