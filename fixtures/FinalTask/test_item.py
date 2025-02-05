e37c9646b5c40379c6090eac68467b91526e1c5b
Илья <vitilyusha@gmail.com>
2025-02-05 15:55:43 +0300

    rename function

--- fixtures/FinalTask/test_item.py
+++ fixtures/FinalTask/test_item.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_bucket_butt(browser):
+def test_guest_should_see_bucket_button(browser):  # поменял название с не очень хорошего, на более сносное)))
     browser.get(link)
     time.sleep(30)
     try:
@@ -17,5 +17,5 @@ def test_guest_should_see_bucket_butt(browser):
     except NoSuchElementException:
         assert False, "NoSuchElementException: Add to cart button is not found on the page"
 
-    # ass_butt_busk = browser.find_element(By.CSS_SELECTOR, ".btn-primary")
+    # ass_button_busk = browser.find_element(By.CSS_SELECTOR, ".btn-primary")
     # assert ass_butt_busk is not None, "Не найдена"   #крайне плохой вариант, не дающий достоверной инфы, к тому же если делать через find_elements - идейность тоже затирается
